663593819296b42fa4f95bfaec8c8fed69911f30
pavan1269 <36643078+pavan1269@users.noreply.github.com>
2020-01-28 11:42:10 +0530

    updated line

--- samp.java
+++ samp.java
@@ -3,6 +3,6 @@ public class samp
 	public static void main(String args[])
 	{
 		System.out.println("Hi this is pavan");
-		System.out.println("Added new line");
+		System.out.println("changed new line");
 	}
 }
